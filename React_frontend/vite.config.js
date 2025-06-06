8697fc672bab66904d9c5a690fc6f8bdae8f9cc2
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-07 04:59:32 +0530

    commit

--- React_frontend/vite.config.js
+++ React_frontend/vite.config.js
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: './', // 👈 This is the key line to fix the white screen!
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"), // This maps '@' to 'src' directory
+      "@": resolve(__dirname, "src"),
     },
   },
-  assetsInclude: ['**/*.svg'], // Ensures SVG files are handled correctly
+  assetsInclude: ['**/*.svg'],
 });
