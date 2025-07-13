c4778109bff27e024fc608670869b84223b53da5
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-07-14 02:15:43 +0530

    commit

--- drf_backend/api/utils/jwt_auth.py
+++ drf_backend/api/utils/jwt_auth.py
@@ -1,6 +1,39 @@
-# api/utils/jwt_auth.py
+
 import jwt
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 from decouple import config
+from bson import ObjectId
+from api.db import users_collection
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth_header = request.headers.get('Authorization')
+
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return None  # Means DRF will continue to other authenticators if any
+
+        token = auth_header.split(" ")[1]
+
+        try:
+            payload = jwt.decode(token, config("ACCESS_TOKEN_SECRET"), algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Token expired")
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed("Invalid token")
+
+        user_id = payload.get("id")  # or "user_id" depending on what you encoded
+        if not user_id:
+            raise AuthenticationFailed("Invalid payload: missing user_id")
+
+        user = users_collection.find_one({"_id": ObjectId(user_id)})
+        if not user:
+            raise AuthenticationFailed("User not found")
+
+        # DRF requires returning (user, auth_token)
+        # You can return the full user dict (or a custom wrapper)
+        return (user, token)
 
 def get_user_from_request(request):
     auth = request.headers.get("Authorization", None)
--- drf_backend/backend/settings.py
+++ drf_backend/backend/settings.py
@@ -72,7 +72,14 @@ MIDDLEWARE = [
 # ------------------------------
 
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'api.exceptions.custom_exception_handler'
+    'EXCEPTION_HANDLER': 'api.exceptions.custom_exception_handler',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BaseAuthentication',
+        'api.utils.jwt_auth.JWTAuthentication',  #  Custom JWT auth agar use kar rahe ho
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Har route protected by default
+    ]
 }
 
 # ------------------------------
